fb3f65a22a631ffccab2b4fe9fdc774b5fd9253f
steel25baby <bridgetgitonga73@gmail.com>
2025-06-22 17:10:22 +0300

    made a few changes to the footer

--- src/pages/homepage/Aboutus.jsx
+++ src/pages/homepage/Aboutus.jsx
@@ -1,9 +1,52 @@
 import React from 'react'
 
-const Aboutus = () => {
+const AboutUs = () => {
   return (
-    <div>Aboutus</div>
+     <section className="about">
+      <div className="Aboutheading">
+        {" "}
+        <h1>About Us</h1>
+      </div>
+      <div className="Topics">
+        <div className="Topic1">
+          <h1>01</h1>
+          <h2>Tour packages</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            autem doloremque obcaecati dolores quia vero vitae laborum ullam,
+            quaerat mollitia!
+          </p>
+        </div>
+        <div className="Topic1">
+          <h1>02</h1>
+          <h2>Hotel Booking</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            autem doloremque obcaecati dolores quia vero vitae laborum ullam,
+            quaerat mollitia!
+          </p>
+        </div>
+        <div className="Topic1">
+          <h1>03</h1>
+          <h2>Camping cites</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            autem doloremque obcaecati dolores quia vero vitae laborum ullam,
+            quaerat mollitia!
+          </p>
+        </div>
+        <div className="Topic1">
+          <h1>04</h1>
+          <h2>Destinations</h2>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum
+            autem doloremque obcaecati dolores quia vero vitae laborum ullam,
+            quaerat mollitia!
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default Aboutus
+export default AboutUs
--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import './homepage.css'
 import {Link} from "react-router-dom"
 import Hero from './Hero'
-import Aboutus from './Aboutus'
+// import Aboutus from './AboutUs'
 import Testimonials from './Testimonials'
 const homepage = () => {
   return (
     <section className='home'>
       <Hero />
-      <Aboutus />
+      {/* <AboutUs /> */}
       <Testimonials />
 
     </section>
